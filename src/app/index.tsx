42f85b2c0331c750d289ed2e8c766f2acc88de67
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-12-11 15:08:51 -0300

    feat: adicionando conteudo da aula 1

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
 import { View, Text } from "react-native"
+import { Welcome } from "@/components/welcome"
+import { Steps } from "@/components/steps"
 
 export default function Index(){
     return (
-        <View>
-            <Text>Hello React Native</Text>
+        <View style={{ 
+            flex: 1, 
+            padding: 40,
+            gap: 40
+        }}>
+            <Welcome />
+            <Steps />
         </View>
     )
 }
